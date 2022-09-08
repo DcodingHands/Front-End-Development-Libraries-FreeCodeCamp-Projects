8bebb0ef257c71bcdb092c73cfe4962c397ab552
DcodingHands <dcodinghands@gmail.com>
2022-09-08 16:40:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [quotes,setQuotes] = useState([])
+  const [randomQuote,setRandomQuotes] = useState('')
+  const getQuote = async ()=> {
+    const apiFetc = await fetch("https://type.fit/api/quotes")
+    const results = await apiFetc.json()
+    setQuotes(results)
+    let randIndx = Math.floor(Math.random() * results.length)
+    setRandomQuotes(results[randIndx])
+  }
+  useEffect(()=>{
+    getQuote() 
+  },[])
+const getNewQuote =()=>{
+  let randIndx = Math.floor(Math.random() * quotes.length)
+  setRandomQuotes(quotes[randIndx])
+}
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="quote-box">
+        <p id="text"><i class="fa fa-quote-left" aria-hidden="true"></i> {randomQuote.text}</p>
+        <p id="author">- {randomQuote.author}</p>
+        <div className="content">
+            <a href="twitter.com/intent/tweet" target="_blank" id="tweet-quote">t</a>
+            <button id="new-quote" onClick={getNewQuote}>New Quote</button>
+        </div>
+      </div>
     </div>
   );
 }
